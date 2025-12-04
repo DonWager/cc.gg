21619bb9eac9e0d014bd0eabbe60c367d5b7c2ac
Torben P. Wager <85026197+DonWager@users.noreply.github.com>
2025-12-04 12:15:07 +0100

    Create seedIndex.lua

---
+++ seedIndex.lua
@@ -0,0 +1,6 @@
+return {
+    Inferium_1 = {row="front",pos="1",dir="left",light=peripheral.wrap("redstone_relay_1")},
+    Inferium_2 = {row="front",pos="1",dir="left",light=peripheral.wrap("redstone_relay_1")},
+    Inferium_3 = {row="front",pos="2",dir="right",light=peripheral.wrap("redstone_relay_1")},
+    Inferium_4 = {row="front",pos="2",dir="left",light=peripheral.wrap("redstone_relay_1")}
+}
